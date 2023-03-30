dc8702b731c934a8daeb2bede040b9d395d4354d
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-03-29 22:43:44 -0500

    Video

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -103,7 +103,7 @@
     .row.mt-3
       p Para conocer qué es una librería, sus tipos y características, lo invitamos a ver el siguiente video.
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/K1IhFD06H3M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption VIDEO       
 
 </template>
